088cd11d570dde283cb774db8a72c37be1e258a9
Alexandru <sandubotnari99@gmail.com>
2023-08-18 13:22:36 +0300

    change activity layout

--- app/src/main/java/com/alexander/countryapp/MainActivity.kt
+++ app/src/main/java/com/alexander/countryapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.alexander.countryapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.alexander.countryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.searchButton.setOnClickListener {
+            val countryName = binding.editTextTextPersonName.text.toString()
+            val country:Country = restCountriesApi.getCountryByName(countryName).get(0)
+            binding.countryNameTextView.setText(country.name)
+            binding.capitalTextView.setText(country.capital)
+
+        }
     }
 }
